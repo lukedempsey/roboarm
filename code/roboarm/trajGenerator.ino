5571e233b79f04223b825a4e7496b014755c6aa0
lawfordian <ilawford@student.unimelb.edu.au>
2017-09-20 22:04:11 +1000

    Update trajGenerator.ino

--- code/roboarm/trajGenerator.ino
+++ code/roboarm/trajGenerator.ino
@@ -1,1 +1,16 @@
 
+
+float* PolySolve(x0, xdot0, xf, xdotf, tf) {
+  float coef[4], A[4][4], B[4][1];
+  
+  A = {{1, 0, 0, 0},
+       {0, 1, 0, 0},
+       {1, tf, tf^2, tf^3},
+       {0, 1, 2*tf, 3*tf^2}};
+  B = {{x0},
+       {xdot0},
+       {xf},
+       {xdotf}};
+  
+  //coef = inv(A)*B... shits fucked
+}
